4a8ffc98d459417e676a3546885c4872714a9bab
Vardhman Surana <100058534+VardhmanSurana@users.noreply.github.com>
2024-11-12 21:20:49 +0530

    Update NetworkObserver.js

--- NetworkObserver.js
+++ NetworkObserver.js
@@ -6,18 +6,14 @@ constructor(pending_transactions,cbc)
 this.pending_transactions= pending_transactions;
 this.cbc= cbc;
 }
-//Function to get transaction records of all sender addresses in a blockchain
 getRecords(sendAddr)
 {
 for(const address of sendAddr)
 {
 var initial_balance=
 global.smashingCoin.getBalanceOfAddress(address);
-//Number of Transactions for the blockchain
 var num=0;
-//Total amount to pay based on number of transactions 
 var total_payment_amt= 0;
-//Array to store transaction amounts from this address
 var amts= new Array();
 for(const trans of this.pending_transactions){
 if(trans.fromAddress=== address)
@@ -31,7 +27,6 @@ this.check_DoubleSpendingAttack(total_payment_amt,
 initial_balance, num, address, amts);
 }
 }
-//Function to check for double spending attacks
 check_DoubleSpendingAttack(total, balance, num, address, amts) {
 console.log("\nNumber of pending transactions from address: ");
 console.log(address+" is:"+ num);
