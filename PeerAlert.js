df8170edb08cbe2341841fb07e24395e282de8a6
Vardhman Surana <100058534+VardhmanSurana@users.noreply.github.com>
2024-11-12 21:21:03 +0530

    Update PeerAlert.js

--- PeerAlert.js
+++ PeerAlert.js
@@ -17,7 +17,6 @@ if(v<=this.cbc.blockchain.length- 1)
 this.cbc.blockchain[v+1].precedingHash = null;
 this.cbc.blockchain.splice(v,1);
 }
-//this.indices.splice(this.indices.indexOf(v),1);
 for(var i=0; i<this.indices.length; i++)
 {
 this.indices[i]-=1;
