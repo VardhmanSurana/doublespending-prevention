0f1aae2f02a7b00bb194df59825188769a2fabb3
Vardhman Surana <100058534+VardhmanSurana@users.noreply.github.com>
2024-11-12 21:19:36 +0530

    Update KeyGenerator.js

--- KeyGenerator.js
+++ KeyGenerator.js
@@ -1,13 +1,10 @@
 const EC = require('elliptic').ec;
 const ec = new EC('secp256k1');
 
-// Generate a new key pair
 const key = ec.genKeyPair();
 
-// Get the public and private keys in hexadecimal format
 const publicKey = key.getPublic('hex');
 const privateKey = key.getPrivate('hex');
 
-// Print the keys to the console
 console.log('Private key:', privateKey);
 console.log('Public key:', publicKey);
